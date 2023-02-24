6e0404def2d3c4c3e44e96f32b7b906724c3be1a
Nathan <nathansoo99@gmail.com>
2023-02-24 13:33:32 +1100

    restructured code

---
+++ src/backend/Game.py
@@ -0,0 +1,8 @@
+from .units.Piece import Piece
+
+class Game:
+    def __init__(self):
+        self.piece = Piece()
+
+if __name__ == "__main__":
+    Game()
--- src/game_elements/Game.py
+++
@@ -1,6 +0,0 @@
-class Game:
-    def __init__(self):
-        pass
-
-if __name__ == "__main__":
-    print("Hello World")
