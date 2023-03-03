04facd34eb01304a814d776806f841090cfcf7e9
Nathan <nathansoo99@gmail.com>
2023-03-03 23:46:04 +1100

    added game method prototypes

--- src/backend/Game.py
+++ src/backend/Game.py
@@ -7,21 +7,32 @@ from .Square import Square
 from .GameData import game_data
 
 class Game(object):
-
-    
     def __init__(self):
         """ 
         Initialize the initial game conditions
         """
 
         # Generate empty board
-        board = []
+        self.board = []
         for x in range(0, 16):
             column = []
             for y in range(0, 16):
                 column.append(Square(None, None))
 
-            board.append(column)
+            self.board.append(column)
 
         # Add pieces and calculate combat strength
         initial_state = game_data["setup"]
+    
+    def change_strength(self):
+        pass
+
+    def check_move_valid(self):
+        pass
+
+    def get_board(self):
+        return self.board
+    
+    def move(self):
+        pass
+
